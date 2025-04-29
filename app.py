3adc1db92961ab20b2975cd8771056195d9d4333
kuchiwo <koyas@plala.to>
2025-04-29 17:07:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ num_sections = st.slider("項目数", min_value=2, max_value=10, value=5)
 # ランダムな割合とラベルを作成
 values = np.random.rand(num_sections)
 values /= values.sum()
-labels = [f"項目 {i+1}" for i in range(num_sections)]
+labels = [f"case {i+1}" for i in range(num_sections)]
 
 # グラフ描画
 fig, ax = plt.subplots()
